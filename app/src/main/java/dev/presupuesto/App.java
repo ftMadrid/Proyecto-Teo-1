2647c28e54bad91bd489645dd84c4a887da7101d
ftMadrid <fernandomadridcv04@gmail.com>
2026-08-29 15:10:42 -0600

    Reestructuracion del proyecto con Maven

--- app/src/main/java/dev/presupuesto/App.java
+++ app/src/main/java/dev/presupuesto/App.java
@@ -3,7 +3,7 @@ package dev.presupuesto;
 import dev.presupuesto.frontend.frames.loginFrame;
 import javax.swing.SwingUtilities;
 
-public class Main {
+public class App {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new loginFrame());
